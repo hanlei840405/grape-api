f5afa88fda05d6f2240e289ccc48163a2533a45b
hanlei840405 <hanlei.840405@gmail.com>
2016-09-30 10:44:51 +0800

    init

---
+++ src/main/java/com/fruit/service/api/GenerateIdRemoteService.java
@@ -0,0 +1,15 @@
+package com.fruit.service.api;
+
+/**
+ * Created by hanlei6 on 2016/9/30.
+ */
+public interface GenerateIdRemoteService {
+    /**
+     * 获取分布式自增ID
+     *
+     * @param application
+     * @param module
+     * @return
+     */
+    String get(String application, String module);
+}
